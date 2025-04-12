fc6116ac83ad8a393264e56ea2507460c3c111f5
Nakul Dev <nakuldev1561@gmail.com>
2025-04-12 16:52:11 +0530

    Add URL checker utility with fetch integration for risk assessment

---
+++ utils/urlChecker.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+async function checkUrl(url) {
+    const apiKey = process.env.IPQ_API_KEY;
+    const encodedURL = encodeURIComponent(url);
+    const apiUrl = `https://ipqualityscore.com/api/json/url/${apiKey}/${encodedURL}`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+        if (data.unsafe || data.phishing || data.risk_score > 70) {
+            console.log(`🚨 High risk: ${url}`);
+          } else if (data.redirected && !data.final_url.includes(data.root_domain)) {
+            console.log(`⚠️ Suspicious redirect from ${data.root_domain} to ${data.final_url}`);
+          } else {
+            console.log(`✅ Looks safe: ${url}`);
+          }
+          
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+checkUrl('http://trezorsuites.my.canva.site');
