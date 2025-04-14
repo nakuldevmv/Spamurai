2759971f7d9fe787b06fef24d20acb32dd349024
Nakul Dev <nakuldev1561@gmail.com>
2025-04-14 12:11:26 +0530

    Refactor: Remove commented-out code for cleaner index.js

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
 import connectToInbox from './imapConnect.js';
 connectToInbox();
-// import readline from 'readline';
-// console.log('Spamurai');
-// console.log('1. Connect to inbox');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// let num;
-// rl.question('Enter your choice: ', (answer) => {
-//     num = parseInt(answer, 10);
-//     switch(num){
-//         case 1:connectToInbox();
-//         break;
-//         default:console.log('Invalid option');
-//         break;
-//     }
-//     rl.close();
-// });
-
